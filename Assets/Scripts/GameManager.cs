681ae4322a47b77f8b1586010f28a388bf2bc2fe
Eli Hurtle Whitbread <ewhitbread@sae.au>
2016-09-30 11:43:19 +1000

    Added a couple of scenes and FPSController (and requirements)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager _gameManager;
+
+    void Awake()
+    {
+        _gameManager = this;
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
