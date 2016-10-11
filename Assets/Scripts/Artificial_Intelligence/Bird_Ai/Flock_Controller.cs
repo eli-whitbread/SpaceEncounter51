ec1b0998b109b18f9e608caaa890791bbd155804
James Evan Neal <james.evan.neal@gmail.com>
2016-10-11 17:23:02 +1100

    Added Boids To Project
    Added them into the testScene.
    Still need edits to the code for gameplay/design purposes. Simple fix
    though.

---
+++ Assets/Scripts/Artificial_Intelligence/Bird_Ai/Flock_Controller.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Flock_Controller : MonoBehaviour
+{
+
+    [Range(0.0f, 5.0f)]
+    public float cohRange, sepRange, allRange;
+
+    [Range(0.0f, 50.0f)]
+    public float maxSpeed, maxVeloc;
+
+
+    public float cohAmp, sepAmp, allAmp, flockAmp;
+
+    public Transform[] flockingPoints;
+    public Transform[] swoopingPoints;
+    public Transform birdPrefab;
+    public int spawnNumber;
+    public int totalSwooping = 3;
+    private int currentSwooping = 0;
+
+    private List<GameObject> birdList;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        StartUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChooseRandomSwoop();
+    }
+    private void StartUp()
+    {
+        birdList = new List<GameObject>();
+        SpawnBirds(birdPrefab, spawnNumber);
+        birdList.AddRange(GameObject.FindGameObjectsWithTag("Bird_AI"));
+
+        foreach (GameObject bird in birdList)
+        {
+            foreach (Transform point in flockingPoints)
+            {
+                bird.GetComponent<Vermin_AI>().AddFlockGoal = point;
+            }
+            foreach (Transform point in swoopingPoints)
+            {
+                bird.GetComponent<Vermin_AI>().AddSwoop = point;
+            }
+        }
+    }
+    private void SpawnBirds(Transform prefab, int numBirds)
+    {
+        for (int i = 0; i < numBirds; i++)
+        {
+            Instantiate(prefab, new Vector3(Random.Range(-3, 3), 0, Random.Range(-3, 3)), Quaternion.identity);
+        }
+    }
+    public List<Vermin_AI> getNeighbourFlock(Vermin_AI bird, float range)
+    {
+        List<Vermin_AI> thisFlock = new List<Vermin_AI>();
+
+        foreach (GameObject otherBird in birdList)
+        {
+            if (otherBird.GetComponent<Vermin_AI>() == bird)
+            {
+                continue;
+            }
+
+            if (Vector3.Distance(bird.transform.position, otherBird.transform.position) <= range)
+            {
+                thisFlock.Add(otherBird.GetComponent<Vermin_AI>());
+            }
+        }
+        return thisFlock;
+    }
+    private void ChooseRandomSwoop()
+    {
+        foreach (GameObject bird in birdList)
+        {
+            if (RandomBoolean() && currentSwooping <= totalSwooping)
+            {
+                bird.GetComponent<Vermin_AI>().flocking = false;
+                currentSwooping++;
+            }
+        }
+    }
+    public void MinusSwooper()
+    {
+        currentSwooping--;
+    }
+
+    private bool RandomBoolean()
+    {
+        if (Random.value >= .8)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/Artificial_Intelligence/Bird_Ai/Vermin_AI.cs
@@ -0,0 +1,229 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Vermin_AI : MonoBehaviour
+{
+   
+
+
+    public bool flocking = true;
+
+    public Vector3 newPos, newVeloc, newAcc;
+    //public Transform currentSwoopGoal;
+
+    
+    private int currentFlockTarg = 0;
+    private int currentSwoopTarg = 0;
+    private Flock_Controller flock_Controller;
+    private Vector3 dir;
+    private Quaternion rotation;
+    private float speed = 20;
+    private float turnSpeed = 10;
+    private bool oneTime = false;
+    private bool returnSwoop;
+
+
+
+
+    public Transform AddSwoop { set { swoopPoints.Add(value); } }
+    public Transform AddFlockGoal { set { flockingGoals.Add(value); } }
+    //remember to convert these to arrays in any method yo uuse them
+    public List<Transform> swoopPoints = new List<Transform>();
+    public List<Transform> flockingGoals = new List<Transform>();
+
+    // Use this for initialization
+    void Start()
+    {
+        StartUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    private void StartUp()
+    {
+        newPos = transform.position;
+        newVeloc = new Vector3(Random.Range(-3, 3), 0, Random.Range(-3, 3));
+        flock_Controller = FindObjectOfType<Flock_Controller>();
+    }
+    private void Movement()
+    {
+        if (flocking)
+        {
+            UpdateFlocking();
+        }
+        else if (!flocking)
+        {
+            SwoopMovement();
+            UpdateSwooping();
+        }
+    }
+    private void UpdateFlocking()
+    {
+        float deltaTime = Time.deltaTime;
+
+        newAcc = JoinVectors();
+
+        newAcc = Vector3.ClampMagnitude(newAcc, flock_Controller.maxSpeed);
+
+        newVeloc = newVeloc + newAcc * deltaTime;
+
+        newVeloc = Vector3.ClampMagnitude(newVeloc, flock_Controller.maxVeloc);
+
+        newPos = newPos + newVeloc * deltaTime;
+
+        transform.position = newPos;
+
+        if ((CheckDistanceToPoint(transform.position, flockingGoals[currentFlockTarg].transform.position)) < 3)
+        {
+            ChooseFlockTarget();
+        }
+        if (newVeloc.magnitude > 0)
+        {
+            transform.LookAt(newPos + newVeloc);
+        }
+    }
+    private void SwoopMovement()
+    {
+        if (returnSwoop)
+        {
+            dir = (flockingGoals[currentSwoopTarg].transform.position - transform.position).normalized;
+        }
+        else
+        {
+            dir = (swoopPoints[currentSwoopTarg].transform.position - transform.position).normalized;
+        }
+
+        rotation = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+    }
+    private void UpdateSwooping()
+    {
+        if (!returnSwoop)
+        {
+            ChooseSwoopTarget();
+            if ((CheckDistanceToPoint(transform.position, swoopPoints[currentSwoopTarg].transform.position)) < 3)
+            {
+                if (!returnSwoop)
+                {
+                    returnSwoop = true;
+                }
+            }
+        }
+        else if(returnSwoop && (CheckDistanceToPoint(transform.position, flockingGoals[currentSwoopTarg].transform.position)) <= 5)
+        {
+            flocking = true;
+            oneTime = false;
+            returnSwoop = false;
+            flock_Controller.MinusSwooper();
+
+        }
+    }
+    private void ChooseSwoopTarget()
+    {
+        if (!oneTime)
+        {
+            currentSwoopTarg = Random.Range(0, swoopPoints.Count);
+            oneTime = true;
+        }
+    }
+    private void ChooseFlockTarget()
+    {
+        currentFlockTarg = Random.Range(0, flockingGoals.Count);
+    }
+    private float CheckDistanceToPoint(Vector3 pos1, Vector3 pos2)
+    {
+        float distance = Vector3.Distance(pos1, pos2);
+        return distance;
+    }
+
+    private Vector3 GoalSeeking()
+    {
+        Vector3 goalVector = new Vector3();
+
+        flockingGoals.ToArray();
+
+        goalVector += flockingGoals[currentFlockTarg].transform.position;
+
+        goalVector -= this.newPos;
+
+        goalVector = Vector3.Normalize(goalVector);
+        return goalVector;
+    }
+    private Vector3 Cohesion()
+    {
+        Vector3 cohVector = new Vector3();
+
+        var flock = flock_Controller.getNeighbourFlock(this, flock_Controller.cohRange);
+
+        if (flock.Count == 0)
+        {
+            return cohVector;
+        }
+        foreach (var bird in flock)
+        {
+            cohVector += bird.newPos;
+        }
+
+        cohVector /= flock.Count;
+
+        cohVector = cohVector - this.newPos;
+
+        cohVector = Vector3.Normalize(cohVector);
+        return cohVector;
+    }
+    private Vector3 Seperation()
+    {
+        Vector3 sepVector = new Vector3();
+
+        var flock = flock_Controller.getNeighbourFlock(this, flock_Controller.sepRange);
+
+        if (flock.Count == 0)
+        {
+            return sepVector;
+        }
+        foreach (var bird in flock)
+        {
+            Vector3 towardsThis = this.newPos - bird.newPos;
+
+            if (towardsThis.magnitude > 0)
+            {
+                sepVector += towardsThis.normalized / towardsThis.magnitude;
+            }
+        }
+
+        sepVector = Vector3.Normalize(sepVector);
+        return sepVector;
+    }
+    private Vector3 Allignment()
+    {
+        Vector3 allVector = new Vector3();
+
+        var flock = flock_Controller.getNeighbourFlock(this, flock_Controller.allRange);
+
+        if (flock.Count == 0)
+        {
+            return allVector;
+        }
+
+        foreach (var bird in flock)
+        {
+            allVector += bird.newVeloc;
+        }
+
+        allVector = Vector3.Normalize(allVector);
+        return allVector;
+    }
+    private Vector3 JoinVectors()
+    {
+        Vector3 joint = new Vector3();
+
+        joint = Cohesion() * flock_Controller.cohAmp + Seperation() * flock_Controller.sepAmp + GoalSeeking() * flock_Controller.flockAmp + Allignment() * flock_Controller.allAmp;
+        return joint;
+    }
+}
