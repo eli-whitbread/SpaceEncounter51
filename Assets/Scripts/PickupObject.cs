3b4160c7be325d835d42282d554f387ac25e3abb
Elliot Moule <ellimyt@live.com.au>
2016-10-12 23:44:22 +1100

    Quick object pickup script. Slightly buggy, I think it's due to how the camera works in this - will need to double check.

---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour
+{
+
+    bool carryingObject;
+    GameObject carriedObject;
+    public float distance;
+    public float smooth;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(carryingObject)
+        {
+            carry(carriedObject);
+            checkDrop();
+        }
+        else
+        {
+            pickup();
+        }
+	}
+
+    void carry(GameObject o)
+    {
+        o.transform.position = Vector3.Lerp(o.transform.position, Camera.main.transform.position + Camera.main.transform.forward * distance, Time.deltaTime * smooth);
+        o.transform.rotation = Quaternion.identity;
+    }
+
+    void pickup()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(x, y));
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Pickupable p = hit.collider.GetComponent<Pickupable>();
+                if(p != null)
+                {
+                    carryingObject = true;
+                    carriedObject = p.gameObject;
+                    p.gameObject.GetComponent<Rigidbody>().useGravity = true;
+                }
+            }
+        }
+
+    }
+
+    void checkDrop()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            dropObject();
+        }
+    }
+
+    void dropObject()
+    {
+        carryingObject = false;
+        carriedObject.gameObject.GetComponent<Rigidbody>().useGravity = false;
+        carriedObject = null;
+        
+    }
+}
