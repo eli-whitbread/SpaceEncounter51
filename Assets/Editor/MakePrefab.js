a89d9f7f4d1f4e3c92b8c1c8aa7f75a5303328c6
Elliot David Moule <emoule@sae.au>
2016-10-20 09:21:25 +1000

    Small change in scene, with Camera location/ Player

---
+++ Assets/Editor/MakePrefab.js
@@ -0,0 +1,53 @@
+﻿// Generate a prefab from the selection
+
+@MenuItem ("Project Tools / Make Prefab")
+static function LogSelectedTransformName () 
+{
+    CreatePrefab();
+}
+
+// Validate the menu item defined by the function above.
+// The menu item will be disabled if this function returns false.
+@MenuItem ("Project Tools / Make Prefab", true)
+static function ValidateLogSelectedTransformName () 
+{
+    // Return false if no transform is selected.
+    return Selection.activeTransform != null;
+}
+
+static function CreatePrefab()
+{
+	var selectedObjects : GameObject[] = Selection.gameObjects;
+	for(var go : GameObject in selectedObjects)
+	{
+		var name : String = go.name;	// Store the name of our selection
+		var localPath : String = "Assets/Prefabs/" + name + ".prefab";	// Create the path for the prefab	
+		
+		// Determine whether prefab already exists
+		if(AssetDatabase.LoadAssetAtPath(localPath, GameObject))
+		{
+			//Check for user choice
+			if(EditorUtility.DisplayDialog("Caution with Prefab: " + go.name, "Prefab " + go.name + " already exists. Do you want to overwrite?", "Yes", "No"))
+			{
+				CreateNew(go, localPath);		// Create new prefab
+			}
+		}
+		else
+		{
+			// Create new Prefab
+			CreateNew(go, localPath);	
+		}
+	}	
+}
+
+// Function Creates a new Prefab
+static function CreateNew(selectedObject : GameObject, localPath : String)
+{
+	var prefab : Object = PrefabUtility.CreateEmptyPrefab(localPath);	// Store prefab
+	PrefabUtility.ReplacePrefab(selectedObject, prefab);				// Set Prefab to prefab
+	
+	AssetDatabase.Refresh();		// Refresh database
+	
+	DestroyImmediate(selectedObject);		// Remove the selected object
+	var clone : GameObject = PrefabUtility.InstantiatePrefab(prefab) as GameObject;		// Replace the object with a prefab
+}
---
+++ Assets/Editor/MakeProjectFolder.js
@@ -0,0 +1,30 @@
+﻿// generate folders in our project
+
+import System.IO;
+
+@MenuItem("Project Tools / Make Folders") // In quotation, provide space, and then the shortcut notation i.e. '#&_g' (Shift + Alt + g)
+// menuItem read the first static function
+static function MakeFolder()
+{
+		GenerateFolders();
+}
+
+static function GenerateFolders()
+{
+	var projectPath : String = Application.dataPath + "/";	// Store the Path for the folders
+	
+	// Creating the folders
+	Directory.CreateDirectory(projectPath + "Audio");
+	Directory.CreateDirectory(projectPath + "Materials");
+	Directory.CreateDirectory(projectPath + "Meshes");
+	Directory.CreateDirectory(projectPath + "Fonts");
+	Directory.CreateDirectory(projectPath + "Textures");
+	Directory.CreateDirectory(projectPath + "Resources");
+	Directory.CreateDirectory(projectPath + "Scripts");
+	Directory.CreateDirectory(projectPath + "Shaders");
+	Directory.CreateDirectory(projectPath + "Packages");
+	Directory.CreateDirectory(projectPath + "Prefabs");
+	Directory.CreateDirectory(projectPath + "Scenes");
+	
+	AssetDatabase.Refresh();
+}
