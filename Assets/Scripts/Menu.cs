aa30931f6b5b3172c263f3501505af0eab016057
Benjamin Richard Molenaar <bmolenaar@sae.au>
2016-10-21 14:53:40 +1100

    Basic Main Menu Functions and Skyboxes

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour
+{
+
+    public Canvas MainCavas;
+    public Canvas OptionsCanvas;
+
+    void Awake()
+    {
+        OptionsCanvas.enabled = false;
+    }
+
+    public void OptionsOn()
+    {
+        OptionsCanvas.enabled = true;
+        MainCavas.enabled = false;
+    }
+
+    public void ReturnOn()
+    {
+        OptionsCanvas.enabled = false;
+        MainCavas.enabled = true;
+    }
+	
+    public void LoadOn()
+    {
+        Application.LoadLevel (1);
+    }
+}
