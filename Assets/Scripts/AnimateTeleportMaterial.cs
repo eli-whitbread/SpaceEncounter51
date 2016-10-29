cae23ae5ef86d098b4750fde1976f70ceba751c2
Elliot Moule <ellimyt@live.com.au>
2016-10-29 11:39:57 +1100

    Updated Teleport Texture, and is now animated (offsetting textures)

---
+++ Assets/Scripts/AnimateTeleportMaterial.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateTeleportMaterial : MonoBehaviour
+{
+
+    public float scrollSpeedX = 0.1f;
+    public float scrollSpeedY = 0.1f;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float offsetX = Time.time * scrollSpeedX;
+        float offsetY = Time.time * scrollSpeedY;
+
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+
+    }
+}
