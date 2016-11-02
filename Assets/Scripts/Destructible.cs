7e9bb80f3f90fd695af8f16327ca91d4f7be0986
Eli Whitbread <1003716@student.sae.edu.au>
2016-11-03 10:49:25 +1100

    Added collision detection to the projectiles. new projectile prefab - using particles (not too sure about this one). added "Destructible" Tag and new Destructible Script - any GameObject that has the Destructible Script AND is tagged ad such can be destroyed/take damage by the projectiles

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructible : MonoBehaviour {
+
+    public int health;
+    
+	public void ApplyDamage(int val)
+    {
+        health -= val;
+        if(health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
