a0a1df83e8940f4fa3648084f666735a08dc093b
Benjamin Richard Molenaar <bmolenaar@sae.au>
2016-11-04 14:21:28 +1000

    Changes to MainLevel - new skybox, new terrain, updated assets.

---
+++ Assets/Scripts/LightningFlash.js
@@ -0,0 +1,11 @@
+﻿var amountOfOff = 0.001;
+var amountOfOn = 0.01;
+
+
+function Update() {
+    if (enabled && (Random.value > amountOfOff))
+        enabled = false;
+    else
+        if (Random.value > amountOfOn)
+            enabled = true;
+}
