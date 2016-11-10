9e1f60d43270cd9a0ccbd3109725a3ddfd5de06b
eli-whitbread <1003716@student.sae.edu.au>
2016-11-10 17:32:38 +1100

    Added basic VR_InteractableObject script - changed Gun to be activated/deactivated via button (currently near gun - look at the red button and hit the E key)

---
+++ Assets/Scripts/VR_InteractableObject.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VR_InteractableObject : MonoBehaviour {
+
+    public enum InteractableObjectType { Pickup, Switch, Gun};
+    public InteractableObjectType objectType;
+
+    public GameObject secondaryObject;
+
+
+	public void Activate()
+    {
+        switch (objectType)
+        {
+            case InteractableObjectType.Pickup:
+                break;
+            case InteractableObjectType.Switch:
+                break;
+            case InteractableObjectType.Gun:
+                GunShootScript shootScript = secondaryObject.GetComponent<GunShootScript>();
+                if(shootScript != null)
+                {
+                    shootScript.EnableGunControl();
+                    secondaryObject.GetComponent<GunAiming>().GunActive = true;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Deactivate()
+    {
+        switch (objectType)
+        {
+            case InteractableObjectType.Pickup:
+                break;
+            case InteractableObjectType.Switch:
+                break;
+            case InteractableObjectType.Gun:
+                GunShootScript shootScript = secondaryObject.GetComponent<GunShootScript>();
+                if (shootScript != null)
+                {
+                    shootScript.DisableGunControl();
+                    secondaryObject.GetComponent<GunAiming>().GunActive = false;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
