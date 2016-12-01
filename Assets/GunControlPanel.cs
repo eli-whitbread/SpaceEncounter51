3833e438f1a1486595760b074c10c97e3e4145d6
James Evan Neal <james.evan.neal@gmail.com>
2016-12-01 18:31:04 +1100

    Added bird count to cannon section

---
+++ Assets/GunControlPanel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GunControlPanel : MonoBehaviour {
+
+    public GameObject panel;
+    public Text panelText;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        panelText.text = GameManager._gameManager.birdsDestroyed.ToString() + "/10";
+        
+	}
+}
